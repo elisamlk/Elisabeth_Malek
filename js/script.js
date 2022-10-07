7ca351ca1b41962941fe62416480461bebaf63b7
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2022-10-07 16:06:48 +0200

    Ajout du fichier php pour le formulaire de contact

--- js/script.js
+++ js/script.js
@@ -1,89 +1,119 @@
 class StickyNavigation {
-    constructor() {
-      this.currentId = null;
-      this.currentTab = null;
-      this.tabContainerHeight = 70;
-      let self = this;
-      $(".et-hero-tab").click(function () {
-        self.onTabClick(event, $(this));
-      });
-      $(window).scroll(() => {
-        this.onScroll();
-      });
-      $(window).resize(() => {
-        this.onResize();
-      });
-    }
-  
-    onTabClick(event, element) {
-      event.preventDefault();
-      let scrollTop =
-        $(element.attr("href")).offset().top - this.tabContainerHeight + 1;
-      $("html, body").animate({ scrollTop: scrollTop }, 600);
-    }
-  
-    onScroll() {
-      this.checkTabContainerPosition();
-      this.findCurrentTabSelector();
-    }
-  
-    onResize() {
-      if (this.currentId) {
-        this.setSliderCss();
-      }
-    }
-  
-    checkTabContainerPosition() {
-      let offset =
-        $(".et-hero-tabs").offset().top +
-        $(".et-hero-tabs").height() -
-        this.tabContainerHeight;
-      if ($(window).scrollTop() > offset) {
-        $(".et-hero-tabs-container").addClass("et-hero-tabs-container--top");
-      } else {
-        $(".et-hero-tabs-container").removeClass("et-hero-tabs-container--top");
-      }
-    }
-  
-    findCurrentTabSelector(element) {
-      let newCurrentId;
-      let newCurrentTab;
-      let self = this;
-      $(".et-hero-tab").each(function () {
-        let id = $(this).attr("href");
-        let offsetTop = $(id).offset().top - self.tabContainerHeight;
-        let offsetBottom =
-          $(id).offset().top + $(id).height() - self.tabContainerHeight;
-        if (
-          $(window).scrollTop() > offsetTop &&
-          $(window).scrollTop() < offsetBottom
-        ) {
-          newCurrentId = id;
-          newCurrentTab = $(this);
-        }
-      });
-      if (this.currentId != newCurrentId || this.currentId === null) {
-        this.currentId = newCurrentId;
-        this.currentTab = newCurrentTab;
-        this.setSliderCss();
-      }
-    }
-  
-    setSliderCss() {
-      let width = 0;
-      let left = 0;
-      if (this.currentTab) {
-        width = this.currentTab.css("width");
-        left = this.currentTab.offset().left;
-      }
-      $(".et-hero-tab-slider").css("width", width);
-      $(".et-hero-tab-slider").css("left", left);
+  constructor() {
+    this.currentId = null;
+    this.currentTab = null;
+    this.tabContainerHeight = 70;
+    let self = this;
+    $(".et-hero-tab").click(function () {
+      self.onTabClick(event, $(this));
+    });
+    $(window).scroll(() => {
+      this.onScroll();
+    });
+    $(window).resize(() => {
+      this.onResize();
+    });
+  }
+
+  onTabClick(event, element) {
+    event.preventDefault();
+    let scrollTop =
+      $(element.attr("href")).offset().top - this.tabContainerHeight + 1;
+    $("html, body").animate({ scrollTop: scrollTop }, 600);
+  }
+
+  onScroll() {
+    this.checkTabContainerPosition();
+    this.findCurrentTabSelector();
+  }
+
+  onResize() {
+    if (this.currentId) {
+      this.setSliderCss();
     }
   }
-  
-  new StickyNavigation();
 
-  $('h1 h2').delay(500).animate({
-    'opacity':'1',
-    'top': '50%'
-  },1000);
+  checkTabContainerPosition() {
+    let offset =
+      $(".et-hero-tabs").offset().top +
+      $(".et-hero-tabs").height() -
+      this.tabContainerHeight;
+    if ($(window).scrollTop() > offset) {
+      $(".et-hero-tabs-container").addClass("et-hero-tabs-container--top");
+    } else {
+      $(".et-hero-tabs-container").removeClass("et-hero-tabs-container--top");
+    }
+  }
+
+  findCurrentTabSelector(element) {
+    let newCurrentId;
+    let newCurrentTab;
+    let self = this;
+    $(".et-hero-tab").each(function () {
+      let id = $(this).attr("href");
+      let offsetTop = $(id).offset().top - self.tabContainerHeight;
+      let offsetBottom =
+        $(id).offset().top + $(id).height() - self.tabContainerHeight;
+      if (
+        $(window).scrollTop() > offsetTop &&
+        $(window).scrollTop() < offsetBottom
+      ) {
+        newCurrentId = id;
+        newCurrentTab = $(this);
+      }
+    });
+    if (this.currentId != newCurrentId || this.currentId === null) {
+      this.currentId = newCurrentId;
+      this.currentTab = newCurrentTab;
+      this.setSliderCss();
+    }
+  }
+
+  setSliderCss() {
+    let width = 0;
+    let left = 0;
+    if (this.currentTab) {
+      width = this.currentTab.css("width");
+      left = this.currentTab.offset().left;
+    }
+    $(".et-hero-tab-slider").css("width", width);
+    $(".et-hero-tab-slider").css("left", left);
+  }
+}
+
+new StickyNavigation();
+
+//Contact Form in PHP
+const form = document.querySelector("form"),
+  statusTxt = form.querySelector(".button-area span");
+form.onsubmit = (e) => {
+  e.preventDefault();
+  statusTxt.style.color = "#0D6EFD";
+  statusTxt.style.display = "block";
+  statusTxt.innerText = "Sending your message...";
+  form.classList.add("disabled");
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "message.php", true);
+  xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      let response = xhr.response;
+      if (
+        response.indexOf("required") != -1 ||
+        response.indexOf("valid") != -1 ||
+        response.indexOf("failed") != -1
+      ) {
+        statusTxt.style.color = "red";
+      } else {
+        form.reset();
+        setTimeout(() => {
+          statusTxt.style.display = "none";
+        }, 3000);
+      }
+      statusTxt.innerText = response;
+      form.classList.remove("disabled");
+    }
+  };
+  let formData = new FormData(form);
+  xhr.send(formData);
+};
