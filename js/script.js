4398c8f14ef2177932b4619bca60facf9ed0ee99
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2022-10-10 16:01:25 +0200

    Mise en place des skills partie desktop

--- js/script.js
+++ js/script.js
@@ -82,3 +82,47 @@ class StickyNavigation {
 }
 
 new StickyNavigation();
+
+(function () {
+  $("ul")
+    .find("svg")
+    .each(function (i) {
+      var c, cbar, circle, percent, r;
+      circle = $(this).children(".cbar");
+      r = circle.attr("r");
+      c = Math.PI * (r * 2);
+      percent = $(this).parent().data("percent");
+      cbar = ((100 - percent) / 100) * c;
+      circle.css({
+        "stroke-dashoffset": c,
+        "stroke-dasharray": c,
+      });
+      circle.delay(i * 150).animate(
+        {
+          strokeDashoffset: cbar,
+        },
+        1000,
+        "linear",
+        function () {
+          return circle.css({
+            "transition-duration": ".3s",
+          });
+        }
+      );
+      $(this)
+        .siblings("small")
+        .prop("Counter", 0)
+        .delay(i * 150)
+        .animate(
+          {
+            Counter: percent,
+          },
+          {
+            duration: 1000,
+            step: function (now) {
+              return $(this).text(Math.ceil(now) + "%");
+            },
+          }
+        );
+    });
+}.call(this));
